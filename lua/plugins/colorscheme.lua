6f67ca44aae7c234d6593a76904867f793984b3d
Syadon <8044537+Syadon@users.noreply.github.com>
2025-11-24 23:28:52 +0100

    Base dotfiles

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    'xiyaowong/transparent.nvim',
+    lazy = false,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    pryority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'mocha',
+        transparent_background = vim.g.transparent_enabled,
+        auto_integrations = true,
+      }
+
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
+  },
+}
+
+--[[
+{
+    -- If you want to see what colorschemes are already installed, you can use `:elescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  }
+--]]
---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+    enabled = true,
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    enabled = true,
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    config = function()
+      require('render-markdown').setup {
+        completions = { lsp = { enabled = true } },
+      }
+    end,
+  },
+}
