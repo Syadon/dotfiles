5babc7676031ababc003f01b651ce17bddbd6e91
Syadon <8044537+Syadon@users.noreply.github.com>
2025-12-01 17:56:58 +0100

    Add more languages to Treesitter ensure_installed

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -4,7 +4,31 @@ return { -- Highlight, edit, and navigate code
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
-    ensure_installed = { 'bash', 'c', 'cpp', 'python', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'latex' },
+    ensure_installed = {
+      'bash',
+      'c',
+      'cpp',
+      'python',
+      'diff',
+      'html',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'query',
+      'vim',
+      'vimdoc',
+      'latex',
+      'css',
+      'dockerfile',
+      'make',
+      'cmake',
+      'json',
+      'javascript',
+      'typescript',
+      'sql',
+      'gdscript',
+    },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
